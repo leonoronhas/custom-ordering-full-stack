fd87ca5c09a5c59c346d062307564fb4fbbe2da6
Shawn C. Lilly <shawnlilly00@yahoo.com>
2020-11-03 21:00:39 -0700

    added to index

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// This will be our main file
+const path = require("path");
+const express = require("express");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const csrf = require("csurf");
+const cors = require("cors");
+
+require("dotenv/config");
+const PORT = process.env.PORT || process.env.DBDEV_PORT; // So we can run on heroku || (OR) localhost:port
+
+const index = express();
+
+// Template engine EJS
+index.set("view engine", "ejs");
+index.set("views", "views");
+ 
+// Middlewares
+index.use(bodyParser.urlencoded({ extended: false }));
+index.use(express.static(path.join(__dirname, "public")));
+
+// Connect to DB
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => {
+    index.listen(PORT, () => {
+      console.log(`DB connected successfully`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
