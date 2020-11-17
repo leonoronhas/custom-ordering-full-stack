b40ef85efb9fb17a8812d95e7226ef490ff03ec2
Thorhian <thorhian@protonmail.com>
2020-11-17 12:05:04 -0700

    Fixed bug for when no session exists. Added check.

--- util/auth.js
+++ util/auth.js
@@ -1,7 +1,7 @@
 //Middleware Functions for authentication checks.
 
 exports.userLoginCheck = (req, res, next) => {
-    if(req.session.user) {
+    if(req.session && req.session.user) {
         next();
     } else {
         res.redirect('/auth/login')
