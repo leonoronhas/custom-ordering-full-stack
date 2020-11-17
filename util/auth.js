3523ff1bb2c1f88c511c4afc77823fc46d7cbb88
Thorhian <thorhian@protonmail.com>
2020-11-17 11:29:01 -0700

    Added authentication helper file.
    The first function added to this is to check to see
    if the user is logged in on the session.

---
+++ util/auth.js
@@ -0,0 +1,9 @@
+//Middleware Functions for authentication checks.
+
+exports.userLoginCheck = (req, res, next) => {
+    if(req.session.user) {
+        next();
+    } else {
+        res.redirect('/auth/login')
+    }
+}
