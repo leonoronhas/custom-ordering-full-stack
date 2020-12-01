fce13f5a224ef522c555f3ec1e414858494958a0
Leonardo <leosud099@gmail.com>
2020-11-30 19:20:08 -0700

    Added route protection for not logged in users

--- routes/projects.js
+++ routes/projects.js
@@ -6,22 +6,29 @@
  * POST:
  *      Create Project
  *******************************************************************/
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { check, body } = require('express-validator');
+const { check, body } = require("express-validator");
 
-const projectController = require('../controllers/projectController');
+const projectController = require("../controllers/projectController");
+const isAuth = require("../middleware/is-auth"); // Protected route
 
-const multerUtil = require('../util/multerUtil');
-const authUtil = require('../util/auth');
+const multerUtil = require("../util/multerUtil");
+const authUtil = require("../util/auth");
 
-router.get('/createProject',
-           authUtil.userLoginCheck,
-           projectController.getCreateProject);
+router.get(
+  "/createProject",
+  isAuth,
+  authUtil.userLoginCheck,
+  projectController.getCreateProject
+);
 
-router.post('/createProject',
-            authUtil.userLoginCheck,
-            multerUtil.projectUploadLocal.array('projectFiles', 8),
-            projectController.postCreateProject);
+router.post(
+  "/createProject",
+  isAuth,
+  authUtil.userLoginCheck,
+  multerUtil.projectUploadLocal.array("projectFiles", 8),
+  projectController.postCreateProject
+);
 
 module.exports = router;
