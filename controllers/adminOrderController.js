1aee9424fbce2cac84d283ca554b1a9af7c094a2
Leonardo <leosud099@gmail.com>
2020-12-03 12:19:31 -0700

    Added comments

--- controllers/adminOrderController.js
+++ controllers/adminOrderController.js
@@ -1,31 +1,29 @@
-
-const project = require("../models/project.js");
 const Project = require("../models/project.js");
 
+// Get pending projects that need a quote price
 exports.getPendingProjects = (req, res, next) => {
-    Project.find({quotePrice: null}).then(projects => {
-        
-     console.log(projects);
-
-        res.render("admin/pending-projects", {
-            projects: projects,
-            pageTitle: "Pending Projects",
-            path: "/admin/pending-projects"
-        })
-    })
+  Project.find({ quotePrice: null }).then((projects) => {
+    res.render("admin/pending-projects", {
+      projects: projects,
+      pageTitle: "Pending Projects",
+      path: "/admin/pending-projects",
+    });
+  });
 };
 
+// Update project with quote price and remove from view
 exports.postPendingProjects = (req, res, next) => {
-    const quotePrice = req.body.price;
-    const projectId = req.body.projectId;
+  const quotePrice = req.body.price;
+  const projectId = req.body.projectId;
 
-    Project.findById(projectId)
-    .then(project => {
-        project.quotePrice = quotePrice;
+  Project.findById(projectId)
+    .then((project) => {
+      project.quotePrice = quotePrice;
 
-        return project.save().then(result => {
-            console.log("Project Updated");
-            res.redirect("/admin/pending-projects");
-        })
-    }).catch(err => console.log(err));
+      return project.save().then((result) => {
+        console.log("Project Updated");
+        res.redirect("/admin/pending-projects");
+      });
+    })
+    .catch((err) => console.log(err));
 };
