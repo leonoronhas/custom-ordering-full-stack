081f07e581d415bf3a2c4f7e197e74769bda8e1d
ManahariDahal <manahari.dahal1@gmail.com>
2020-12-05 14:22:00 -0700

    account

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -2,7 +2,6 @@ const Project = require("../models/project.js");
 const Order = require('../models/order');
 
 exports.getAccount = (req, res, next) => {
-<<<<<<< HEAD
 		const userEmail = req.body.email;
 		const password = req.body.password;
 
@@ -12,11 +11,8 @@ exports.getAccount = (req, res, next) => {
 	        email: userEmail,
 	        password: password,
 	});
-};
-=======
     res.render("account/account", {
       pageTitle: "Account",
       path: "/account",
     });
-  };
->>>>>>> 8fbbddd07af98d7f4109c1ea0c222e1cb455f8b1
+}
