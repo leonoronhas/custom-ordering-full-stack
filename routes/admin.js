eaa1adfd5509e56de7bdb900cb5cf6a924905e07
Leonardo <leosud099@gmail.com>
2020-12-05 23:29:14 -0700

    Approved projects implementation done

--- routes/admin.js
+++ routes/admin.js
@@ -10,6 +10,9 @@ const isAuth = require("../middleware/is-auth");
 const router = express.Router();
 
 router.get("/pending-projects", isAuth, adminController.getPendingProjects);
+
 router.post("/pending-projects", isAuth, adminController.postPendingProjects);
+
+router.get("/approved-projects", isAuth, adminController.getApprovedProjects);
 
 module.exports = router;
